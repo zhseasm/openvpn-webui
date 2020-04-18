055394178b0b16476a8babecf1f0e9a5226b3336
root <1823590952@qq.com>
2020-04-19 01:00:36 +0800

    modified:   env/manageinstall.sh

--- env/manageinstall.sh
+++ env/manageinstall.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+dirfile=$(find / -name "openvpn-webui")
 cd /var/www/html
-git clone https://github.com/zhseasm/openvpn-webui.git
-mv openvpn-web/* .
+#git clone https://github.com/zhseasm/openvpn-webui.git
+mv $dirfile/* .
 cd /var/www/html/sql
 mysql --connect-expired-password -uroot -ptoor -e "create database rbac;"
 mysql -uroot -ptoor rbac <rbac.sql

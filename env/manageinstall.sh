de2f3e1d5eb85da712daf9da2d81b8c028866f7a
root <1823590952@qq.com>
2020-04-18 21:43:38 +0800

    modified:   env/manageinstall.sh 	modified:   view/novnc.php

--- env/manageinstall.sh
+++ env/manageinstall.sh
@@ -1,5 +1,5 @@
 cd /var/www/html
-git clone https://github.com/zhseasm/openvpn-web.git
+git clone https://github.com/zhseasm/openvpn-webui.git
 mv openvpn-web/* .
 cd /var/www/html/sql
 mysql --connect-expired-password -uroot -ptoor -e "create database rbac;"
